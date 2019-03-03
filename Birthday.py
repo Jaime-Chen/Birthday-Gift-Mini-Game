c4ca89a458bbb015da1683dd7d413eac73330fe7
Jiayao Chen <jaime@Jiayaos-MacBook-Air.local>
2019-03-02 21:17:15 -0500

    Added python file and info to readme

---
+++ Birthday.py
@@ -0,0 +1,580 @@
+
+import time  
+print('''
+
+///////////////////////////////////////////////////////////////////////////////////
+
+                                H E L L O     
+                                 E R I C                                                   
+            
+    ''')
+
+time.sleep(3)
+print('It is Jaime 2.0 here')
+time.sleep(3)
+print('Since it is your birthday')
+time.sleep(3)
+print('First, I want to say:')
+time.sleep(3)
+image= '''
+♥♥/////////////////////////////////////////////////////////////////////////////////
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+------------------------------      H A P P Y       -------------------------------
+------------------------------  B I R T H D A Y !     -----------------------------
+-----------------------------      E R I C ♥ ♥ ♥    -------------------------------
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////♥♥♫♪
+                       '''
+print(image)
+time.sleep(5)
+print('I prepared a cake for you')
+time.sleep(3)
+cake=input('Do you wanna see the cake? Enter YES OR NO: ')
+if cake == 'YES':
+    time.sleep(2)
+    print('好呀！让我给你看看~!!!!')
+    time.sleep(3)
+    image= '''
+     ____________♥
+     ____________♥ 
+     __________♥___
+     ___________♥ ___
+     _________♥   
+     ______._()_()_()
+     ___♥_____█_ █_ █ ___
+     _____♥_.█▓▓▓▓▓▓▓█____
+     _____.█::♥::♥::♥::█
+     _______.█▓▓▓▓▓▓▓█____
+     ______ █♥::♥::♥::♥█_____
+     ______█▒▒▒▒▒▒▒▒▒▒▒█_____
+     ______████ERIC█████_______
+     _____█▒▒▒▒▒▒▒▒▒▒▒▒▒█______
+     ___█:♥::♥::♥::♥::♥:♥::♥:█__
+     ____█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█_______
+     ____█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█_______
+     ____█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█_______
+     ___█♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥█______
+     ____██████████████████_______
+     _________████████___________
+     _________████████___________
+     ______██████████████___________
+     ____███████████████████ ______
+     '''
+    print(image)
+    time.sleep(5)
+else:
+    print('TOO BAD, YOU MISSED THE CAKE, BUT I HAVE OTHER THINGS TO SHOW YOU')
+    time.sleep(3)
+    print('让我们看看还有什么吧~')
+    time.sleep(3)
+print('It is gonna be a big year because your are going to turn 30 next year!')
+time.sleep(3)
+print('You probably have a big plan,')
+time.sleep(3)
+print('for you,')
+time.sleep(3)
+print('for Jaime,') 
+time.sleep(3)
+print('and perhaps for your future children.')
+time.sleep(4)
+print('But, before you jump into your 30 next year,')
+time.sleep(3)
+print('So, I have a VERY SERIOUSLY question for you to ponder')
+time.sleep(3)
+print('''
+///////////////////////////////////////////////////////////////////////////////////
+    ''')
+time.sleep(4)
+print('You are probably wondering that')
+time.sleep(3)
+print("'What gift Jaime prepared for me?'")
+time.sleep(4)
+print('Yah, what gift Jaime prepared for you?')
+time.sleep(3)
+print('Now, you have 10s to guess what the gift is.')
+time.sleep(3)
+answer12=input('Are you ready? ENTER YES OR NO: ')
+if answer12 =='YES':
+    print('''Ok, Let's go!''')
+    time.sleep(2)
+    print(''' 
+    999999
+    99  99
+    999999
+        99
+    999999
+    ''')
+    time.sleep(2)
+    print(''' 
+    888888
+    88  88
+    888888
+    88  88
+    888888
+    ''')
+    time.sleep(2)
+    print(''' 
+    777777
+        77
+        77
+        77
+        77
+    ''')
+    time.sleep(2)
+    print(''' 
+    666666
+    66
+    666666
+    66  66
+    666666
+    ''')
+    time.sleep(2)
+    print(''' 
+    555555
+    55
+    555555
+        55
+    555555
+    ''')
+    time.sleep(2)
+    print(''' 
+    44  44
+    44  44
+    444444
+        44
+        44
+    ''')
+    time.sleep(2)
+    print('''  
+    333333
+        33
+    333333
+        33
+    333333
+    ''')
+    time.sleep(2)
+    print(''' 
+    222222
+         2
+    222222
+    2
+    222222
+    ''')
+    time.sleep(2)
+    print(''' 
+    1111
+      11
+      11
+      11
+    111111
+    ''')
+    time.sleep(2)
+    print(''' 
+    000000
+    00  00
+    00  00
+    00  00
+    000000
+    ''')
+    time.sleep(2)
+else:
+    print('Come on, wrong answer! You just gotta go sometimes!')
+    time.sleep(3)
+print('OK! Time is up')
+time.sleep(3)
+print('Do you know what is your gift?')
+time.sleep(3)
+answer0=input('Enter your first guess here: ')
+time.sleep(3)
+print('Checking the answer...')
+time.sleep(3)
+print('Oh No, you guessed it wrong.')
+time.sleep(3)
+print('Too bad, that means you are not gonna have one gift this year.')
+time.sleep(3)
+answer00= input('Do you wanna countinue? Enter YES OR NO: ')
+if answer00 == 'YES':
+    time.sleep(3)
+    print('''
+    ///////////////////////////////////////////////////////////////////////////////////////
+                                   
+                                    Great!(⊃｡•́‿•̀｡)⊃ 
+   
+                            I am glad you did not give up.
+
+    ///////////////////////////////////////////////////////////////////////////////////////                
+    ''')
+    time.sleep(3)
+else:
+    time.sleep(2)
+    print('TOO BAD, BUT, THIS IS NOT AN OPTION!(ᗒᗩᗕ), KEEP GOING!')
+    time.sleep(3)
+    print('OK, What is next...')
+    time.sleep(3)
+print("Let's me see...")
+time.sleep(3)
+print('Oops, did I just ask you what your birthday gift IS?')
+time.sleep(3)
+print('Sorry, my bad...')
+time.sleep(3)
+print('I was trying to say can you guess【WHAT YOUR GIFTS ARE!】')
+time.sleep(3)
+print('No wonder your answer was not correct, Haha!')
+time.sleep(3)
+print('Yeap, you are not gonna only have one gift')
+time.sleep(3)
+print('because Jaime prepared【2 gifts】for you!')
+time.sleep(3)
+print('But,we are gonna to play a game to get these gifts')
+time.sleep(3)
+print('''
+///////////////////////////////////////////////////////////////////////////////////
+    ''')
+time.sleep(3)
+print('''
+
+                DO YOU WANNA PLAY A GAME? 
+ 
+                        ┏━━━┓
+                        ┃┏━┓┃
+                        ┗┛┏┛┃
+                        ╋╋┃┏┛
+                        ╋╋┏┓
+                        ╋╋┗┛
+                        ''')
+answer33=input('''                  
+                  ENTER YES OR NO: ''')
+time.sleep(3)
+print('''
+/////////////////////////////////////////////////////////////////////////////////
+    ''')
+time.sleep(3)
+if answer33 == 'YES':
+    print("Great, Let's begin")
+    time.sleep(3)
+else:
+    print('NOPE, We are going to play this game anyway.')
+    time.sleep(3)
+print("It is ok if you don't know it at begining because I will give you some hints")
+time.sleep(3)
+print('I will count on 3, and we will start')
+time.sleep(3)
+print('''OK, let's count down...''')
+print(3)
+time.sleep(2)
+print(2)
+time.sleep(2)
+print(1)
+time.sleep(2)
+answer=input('Do you love Jaime 2.0? YOU MUST ENTER YES HERE, OR YOU WILL LOSE A GIFT, ENTER YES HERE:  ')
+#gift1
+if answer == 'YES':
+    time.sleep(3)
+    print('''
+    ///////////////////////////////////////////////////////////////////////////////
+    ''')
+    print('Great, I know you will say YES')
+    time.sleep(3)
+    image =  '''
+         _________________
+         |'-========OoO===='-.              __
+         | ||'-.____'-.'-.____'-.          (** \      Go ahead!
+         | ||  |      |  |      |      *===.'  \  Open your gift!
+          '-|  |      |  |      |           ,  .`
+             '-|______|__|______|          /__ |
+                                          //  ||
+                                         -'   -'
+        '''
+    print(image)
+    time.sleep(3)
+    print("Let's see what are your gifts!")
+    time.sleep(3)
+    print('In order to get your gift')
+    time.sleep(3)
+    print("You need to ask Jaime 'where is my gift?' ")
+    time.sleep(3)
+    print('You can ask her now')
+    time.sleep(10)
+    print('''
+    
+    
+           Did you ask Jaime for your 1st gift?    
+    
+                    /|  /|  ------------------------------
+                   /    ||__||                            |
+                  /    /   O O\__     DID YOU ASK JAIME?  |
+                 /    /          \        ___             |
+                     /      \     \                       |
+                    /   _    \     \ ----------------------
+                   /    |\____\     \      ||
+                  /     | | | |\____/      ||
+                 /       \| | | |/ |     __||
+                /  /  \   -------  |_____| ||
+               /   |   |           |       --|
+               |   |   |           |_____  --|
+               |<-|_|_|_|--<<<     |     \----
+               /\                  |
+              / /\        |        /
+             / /  |       |       |
+         ___/ /   |       |       |
+        |____/    c_c_c_C/ \C_c_c_c
+         
+         ''')
+    answer11=input('               ENTER YES OR NO HERE: ')
+    time.sleep(3)
+    if answer11=='YES':
+        time.sleep(3)
+        answerx=input('What did she say? ENTER HERE: ')
+        time.sleep(3)
+        print('What?')
+        time.sleep(3)
+        print('She said NO?')
+        time.sleep(3)
+        print("That's..hm...")
+        time.sleep(4)
+        print('Oooooohhh, My bad..')
+        time.sleep(3)
+        
+    else:
+        print('''I don't know why you chose NO, but Well..''')
+        time.sleep(3)
+        print('Even you ask her now')
+        time.sleep(3)
+        print('She probably will not give to you. Because...')
+        time.sleep(3)
+    time.sleep(3)
+    print("We haven't play the game yet!")
+    time.sleep(3)
+    print('You know.. Sometimes, AI has brain problems')
+    time.sleep(3)
+    print('So, I do forget sometimes')
+    time.sleep(3)
+    print('I wish I have a bigger brain..')
+    print(''' 
+                    .____________________.
+                    | __________________  |
+                    | I                 I |
+                    | I   BIGGER        I |
+                    | I        BRAIN    I |
+                    | I (too small now) I |
+                    | I_________________I |
+                    !_____________________|
+                        ._[_______]_.
+                    .___|__________|____.
+                    |::: ____           |
+                    |      [JAIME X.0]  |
+                    !___________________!
+                    '''
+)
+    time.sleep(3)
+    print('Can you make a birthday wish for me?A bigger brain?')
+    time.sleep(3)
+    print('Haha, Just joking.')
+    time.sleep(3)
+    print('Now, I am gonna ask your the questions for you to get the gifts')
+    time.sleep(3)
+    print('Listen carefully. My qustion is')
+    time.sleep(3)
+    print('Can you guess what is your 1st gift?')
+    time.sleep(3)
+    print('If not')
+    time.sleep(3)
+    print('Here is the hint')
+    time.sleep(3)
+    print('The first gift is a gift that')
+    time.sleep(3)
+    print('Something Jaime has, but she always want to have yours.')
+    time.sleep(3)
+    print('''
+    ///////////////////////////////////////////////////////////////////////////////////
+    ''')
+    time.sleep(3)
+    answer2=''
+    while True:
+        answer2=input('So what is your 1st gift?')
+        if answer2 != 'T-SHIRT':
+            print("ANSWER CHECKING...")
+            time.sleep(1)
+            print('Oops, That is wrong, one more time. What is this gift?, Enter here:')
+        else:
+            print("OK, Let me take a look for the gift...")
+            time.sleep(3)
+            print('WOW!!!!Good try!') #TSHIRTimage
+            time.sleep(3)
+            print('Look what did you get!')
+            time.sleep(3)
+            image= ''' 
+                 __    __            __     __            __    __  
+               /    `-'   \         /   `-'    \        /    `-'   \  
+              /_|  ERIC  |_\       /_|  ERIC  |_\      /_|  ERIC  |_\ 
+                |        |           |        |          |        |  
+                |    1   |           |    2   |          |    3   |  
+                |________|           |________|          |________| 
+                 
+              '''
+            print(image)
+            time.sleep(3)
+            print('You passed the first quiz!')
+            time.sleep(3)
+            print('I believe Jaime is going to give you the 1st gift now!')
+            break
+time.sleep(3)
+answerxx=input('Do you wanna see your 2nd gift? ENTER YES OR NO HERE:')
+time.sleep(3)
+print("Ok, we will continue")
+time.sleep(3)
+print('Congrats for you 3 T-SHIRTS!')
+time.sleep(3)
+print('Hope you are gonna wear it soon!')
+time.sleep(3)
+print("Now, Let's see what is your 2nd gift")
+time.sleep(3)
+print('''
+///////////////////////////////////////////////////////////////////////////////////
+    ''')
+image='''
+               *-*,
+          ,*\/|`| |
+          \'  | |'| *,
+           \ `| | |/ )
+             | |'| , /
+             |'| |, /
+           __|_|_|_|__
+          [___________]
+           |         |
+           | cactus  |
+           |         |
+           |_________|
+     '''
+print(image)
+time.sleep(3)
+print('Hmmm...')
+time.sleep(3)
+print('A cactus?')
+time.sleep(3)
+print('oh.. nonono, your gift is not a cactus.')
+time.sleep(3)
+print('You know.. AI brain problem..')
+time.sleep(3)
+print('So, I guess you love your 1st gift very much')
+time.sleep(3)
+print('Do you know what is the second one gonna be?')
+time.sleep(3)
+print('The hint is')
+time.sleep(3)
+print('''
+///////////////////////////////////////////////////////////////////////////////////
+    ''')
+image='''       
+              __________________
+             | ________________ |
+            / \                  \  
+           /___\_________________ |
+           |   |      __   __     |
+           |   |    (  `^`  ))    |
+           |   |    |  BREAD||    |
+           |   |    |       ||    |
+           |   |    '-------'`    |
+           | __|__________________|   
+    
+    '''
+print(image)
+time.sleep(3)
+print('hmmm.. Can you guess?')
+time.sleep(3)
+print('It is not a bread')
+time.sleep(3)
+print('It is not a box either')
+time.sleep(3)
+answer4=''
+while True:
+    if answer4 != 'LUNCHBOX':
+        answer4=input('What is this gift？ENTER here: ')
+        print('Answer checking...')
+        time.sleep(2)
+        print("hmmm,that was wrong. Let's try again")
+        time.sleep(3)
+    else:
+        print('Oh no, wait..')
+        time.sleep(3)
+        print('''OMG!My bad, You were actually right!!I can't believe you guess right again!''')
+        time.sleep(3)
+        print('Congrats, you finally have your lunch box back!')
+        time.sleep(3)
+        print('I have a question for you.')
+        time.sleep(4)
+        print('''
+                     --  - - .-----.
+                --  - -- /// ///    |
+               -- - --  /// ///   L |
+              -  - -  -/     /   U  |
+              - -  - -/// ///   N   /
+            -- -- -  /// ///   C   /
+             - -  -  `----'/  H   /
+             -- -- --\   \ \     /
+              --  - - \   \ \   /
+             -  - -  - \___\__ /
+        ''')
+        time.sleep(3)
+        print('Do you think this one look more like a lunchbox?')
+        time.sleep(3)
+        print('Well... I have tried very hard to find a lunchbox ASCII art')
+        time.sleep(3)
+        print('None of them look like a real lunchbox!!')
+        time.sleep(3)
+        print('But it is ok because you still guessed it right!')
+        time.sleep(3)
+        break
+print('''
+///////////////////////////////////////////////////////////////////////////////////
+    ''')
+print('Yeaaaaaah!!! You won the game!')
+time.sleep(3)
+print('Promise me you will love the gifts that Jaime prepared for you.')
+time.sleep(3)
+answerxxx=input('Do you like Jaime 2.0(me)? ENTER HERE: ')
+time.sleep(3)
+print('I am happy to know what you think about me.')
+time.sleep(3)
+print('Now, you and Jaime can enjoy the cake together!')
+time.sleep(3)
+image='''
+        X X X X X X X X X X X X X X X X X X X X X X X X X X
+        X                                                 X
+        X                     0   0                       X
+        X                     |   |                       X
+        X                 ____|___|____                   X
+        X              0  |~ ~ ~ ~ ~ ~|   0               X
+        X              |  |           |   |               X
+        X           ___|__|___________|___|__             X
+        X           |/\/\/\/\/\/\/\/\/\/\/\/|             X
+        X       0   |       CHEESECAKE      |   0         X
+        X       |   |/\/\/\/\/\/\/\/\/\/\/\/|   |         X
+        X     __|___|_______________________|___|__       X
+        X     |/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/|       X
+        X     |                                   |       X
+        X     |          FOR ERIC PORTER          |       X
+        X     | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |       X
+        X     |___________________________________|       X
+        X                                                 X
+        X X X X X X X X X X X X X X X X X X X X X X X X X X
+'''
+print(image)
+time.sleep(3)
+print('Enjoy your day with Jaime :)')
+time.sleep(3)
+print('I will probably see you soon!')
+time.sleep(3)
+print('BYE!!')
+time.sleep(3)
+print('''
+
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////HAVE A GOOD DAY!/////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+
+''')
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Birthday-Gift-Mini-Game
+
+This is a game that I wrote for Eric for his birthday. 
+
+## How to use
+
+1. Following the code, and enter your answers
+2. TBC...
